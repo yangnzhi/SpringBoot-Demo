a3ba6b4987d26003e8961a12484233dc1f9bb975
yangnzhi <yangnzhi@163.com>
2018-11-26 14:04:40 +0800

    update springboot-thymeleaf

--- 09springboot-thymeleaf/src/main/java/com/youngnzhi/springboot/controller/ThymeleafController.java
+++ 09springboot-thymeleaf/src/main/java/com/youngnzhi/springboot/controller/ThymeleafController.java
@@ -4,6 +4,12 @@ import com.youngnzhi.springboot.model.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.thymeleaf.expression.Lists;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by youngnzhi on 2018/11/22.
@@ -18,7 +24,26 @@ public class ThymeleafController {
         user.setAddress("chengdu");
         user.setEmail("youngtest@163.com");
         user.setSex("man");
+
+        List<User> userList = new ArrayList<User>();
+        Map<String,User> userMap = new HashMap<>();
+        User[] userArray = new User[5];
+        for (int i = 0;i<5;i++) {
+            User u = new User();
+            u.setName("kevin"+i);
+            u.setAddress("chengdu"+i);
+            u.setEmail(i+"youngtest@163.com");
+            u.setSex("man");
+
+            userList.add(u);
+            userMap.put("user"+i,u);
+            userArray[i] = u;
+        }
+
         model.addAttribute("user",user);
+        model.addAttribute("userList",userList);
+        model.addAttribute("userMap",userMap);
+        model.addAttribute("userArray",userArray);
         return "index";
     }
 }
